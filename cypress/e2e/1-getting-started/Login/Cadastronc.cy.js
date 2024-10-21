4d2a7511823ec51acbde1ca408a80a0402ebe4bc
SambaWml <wes.m.ley@gmail.com>
2024-10-21 11:39:03 -0300

    novo 1

--- cypress/e2e/1-getting-started/Login/Cadastronc.cy.js
+++ cypress/e2e/1-getting-started/Login/Cadastronc.cy.js
@@ -1,12 +1,12 @@
 describe('Cadastro de NC', () => {
     beforeEach(() => {
     });
-    it('Cadastro de NC', () => {
+    it('HEM-1: Cadastro de NC', () => {
         cy.verificaçãoeredirecionamento(); // 
         cy.login(); // Fazer Login Válido
         cy.visualizarTelaDeCadastroNC(); //Ser redirecionado para tela de cadastro NC
         cy.cancelarEmissao();
-        cy.visualizarTelaDeCadastroNC();
+        //cy.visualizarTelaDeCadastroNC(); // Validar se é necessario
         cy.preencherCabecalho(); //Vai ser preenchido a aba do cabeçalho
         cy.preencherObrigacoes(); // Vai preencher a aba obrigações
         cy.enviarPagamentoInvalido(); // Puxar esse e dps fazer o Valido
---
+++ qase.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    token: 'fca01c864c99b974cc841c2849a0e898d969691e5044726baa48c31e746360e0', // Cole sua API Key aqui
+    projectCode: 'HEM', // Substitua pelo código do seu projeto
+    runId: null, // ID da execução, pode ser usado para rodar testes específicos
+    // Outras configurações conforme necessário
+  };
